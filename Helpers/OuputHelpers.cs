b65d811d4d39708bf5a2c87a0f6170d4186a3535
Chris Anthony Lafleur <chris.lafleur246@gmail.com>
2025-12-05 21:32:30 -0500

    Update OuputHelpers.cs

--- Helpers/OuputHelpers.cs
+++ Helpers/OuputHelpers.cs
@@ -8,15 +8,16 @@ namespace Bank_Simulator.Output
 {
     public static class OutputHelpers
     {
-<<<<<<< HEAD
+
         public static void KeyContinue()
         {
             Console.Write("Press any key to continue..."); Console.ReadKey();
-=======
+        }
+
         public static void KeyContinue(string message = "Press any key to continue...")
         {
             Console.Write(message); Console.ReadKey();
->>>>>>> main-menu-helper
+
         }
         public static void ColorError()
         {
@@ -24,15 +25,12 @@ namespace Bank_Simulator.Output
             Console.Write("Error: ");
             Console.ResetColor();
         }
-<<<<<<< HEAD
-        
-=======
+
         public static void ColorNew()
         {
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.Write("New ");
             Console.ResetColor();
         }
->>>>>>> main-menu-helper
     }
 }
